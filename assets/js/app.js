3039e9ef44cfa3dc4c7b992ca055806b16d8343c
BekzahnovR <bekzhanovofficial@gmail.com>
2024-04-17 08:23:49 +0500

    Update

---
+++ assets/js/app.js
@@ -0,0 +1,39 @@
+// Buttons
+const loginBtn = document.querySelector("#loginBtn")
+const registerBtn = document.querySelector("#registerBtn")
+const resetBtn = document.querySelector("#resetBtn")
+
+// Links
+const toForgot = document.querySelector(".toForgot")
+const toRegister = document.querySelector(".toRegister")
+const toLogin = document.querySelector(".toLogin")
+
+// Pages
+const loginPage = document.querySelector(".login")
+const registerPage = document.querySelector(".register")
+const addressPage = document.querySelector(".address")
+const forgotPage = document.querySelector(".forgot")
+
+toRegister.addEventListener("click", () => {
+    loginPage.classList.remove("active")
+    registerPage.classList.add("active")
+})
+
+toForgot.addEventListener("click", () => {
+    loginPage.classList.remove("active")
+    forgotPage.classList.add("active")
+})
+
+toLogin.addEventListener("click",() => {
+    registerPage.classList.remove("active")
+    loginPage.classList.add("active")
+})
+
+registerBtn.addEventListener("click", () => {
+    registerPage.classList.remove("active")
+    addressPage.classList.add("active")
+})
+
+authentication.forEach(element => {
+    element
+});
